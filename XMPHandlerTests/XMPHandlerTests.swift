1fbe5eacb7b46f08091b38401e9718794f60b7de
Frank van Boheemen <frankvb@gmail.com>
2019-07-18 12:12:28 +0200

    Adds testfiles and first couple of tests

--- XMPHandlerTests/XMPHandlerTests.swift
+++ XMPHandlerTests/XMPHandlerTests.swift
@@ -10,8 +10,13 @@ import XCTest
 @testable import XMPHandler
 
 class XMPHandlerTests: XCTestCase {
-
-    let xmpHandler = XMPHandler()
+    
+    private var testFileDir : URL {
+        get {
+            return URL(fileURLWithPath: #file).deletingLastPathComponent().appendingPathComponent("testFiles")
+        }
+    }
+    private let xmpHandler = XMPHandler()
     
 //    override func setUp() {
 //        // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -21,13 +26,34 @@ class XMPHandlerTests: XCTestCase {
 //        // Put teardown code here. This method is called after the invocation of each test method in the class.
 //    }
 
+    func testTestFileDirFound () {
+        XCTAssert(FileManager.default.fileExists(atPath: testFileDir.path))
+    }
+    
     func testParseXMP() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let validXMPFileURL = testFileDir.appendingPathComponent("valid-xmp.xmp")
+        XCTAssert(FileManager.default.fileExists(atPath:validXMPFileURL.path))
+        
+        do {
+            let result = try xmpHandler.parseXMP(from: validXMPFileURL)
+            XCTAssert(result.attributes != nil)
+            XCTAssert(result.dcObjects != nil)
+        } catch let error {
+            print(error)
+        }
+
     }
     
     func testThrowErrorWhenFileIsNotFound() {
+        let brokenURL = testFileDir.appendingPathComponent("brokenURL.xmp")
+        XCTAssert(!FileManager.default.fileExists(atPath:brokenURL.path))
         
+        do {
+            let _ = try xmpHandler.parseXMP(from: brokenURL)
+        } catch let error {
+            print(error)
+            XCTAssert(true)
+        }
     }
     
     func testThrowErrorWhenParsingInvalidXMP() {
